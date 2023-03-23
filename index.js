50a5b77a0355828ad205009f4c5586915dc24df1
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-03-23 10:26:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) { // prevent dodger from going off left edge
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const leftNumbers = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumbers, 10);
+    if (left < 360 - 40) { // prevent dodger from going off right edge
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowLeft') {
+      moveDodgerLeft();
+    } else if (e.key === 'ArrowRight') {
+      moveDodgerRight();
+    }
+  });
+  
 // Your code here
